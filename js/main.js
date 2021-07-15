4de567c3b831e0ac3d631ba2fdfbadc1ef4c749a
MartyInFuture <martyinfuture@gmail.com>
2021-07-15 18:26:26 +0300

    add objects in array

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
 const imageArr = [
-  './images/back-to-the-future1.jpg',
-  './images/back-to-the-future2.jpg',
-  './images/back-to-the-future3.jpg',
-  './images/lord-of-the-ring1.jpg',
-  './images/lord-of-the-rings2.jpg',
-  './images/lord-of-the-rings3.jpg',
-  './images/star-wars-new-hope.jpg',
-  './images/star-wars-empires-strike-back.jpg',
-  './images/star-wars-return-of-the-jedi.jpg',
+  {
+    preview: './images/back-to-the-future1.jpg',
+    original: './images/back-to-the-future01.jpg',
+    description: 'Back to the future 1',
+  },
+  {
+    preview: './images/back-to-the-future2.jpg',
+    original: './images/back-to-the-future02.jpg',
+    description: 'Back to the future 2',
+  },
+  {
+    preview: './images/back-to-the-future3.jpg',
+    original: './images/back-to-the-future03.jpg',
+    description: 'Back to the future 3',
+  },
+  {
+    preview: './images/lord-of-the-ring1.jpg',
+    original: './images/lord-of-the-ring01.jpg',
+    description: 'The lord of the fing 1',
+  },
+  {
+    preview: './images/lord-of-the-rings2.jpg',
+    original: './images/lord-of-the-rings02.jpg',
+    description: 'The lord of the fing 2',
+  },
+  {
+    preview: './images/lord-of-the-rings3.jpg',
+    original: './images/lord-of-the-rings03.jpg',
+    description: 'The lord of the fing3',
+  },
+  {
+    preview: './images/star-wars-new-hope.jpg',
+    original: './images/star-wars-new-hope0.jpg',
+    description: 'Star wars 4',
+  },
+  {
+    preview: './images/star-wars-empires-strike-back.jpg',
+    original: './images/star-wars-empires-strike-back0.jpg',
+    description: 'Star wars 5',
+  },
+  {
+    preview: './images/star-wars-return-of-the-jedi.jpg',
+    original: './images/star-wars-return-of-the-jedi0.jpg',
+    description: 'Star wars 5',
+  },
 ];
 
 const gallery = document.querySelector('.gallery');
@@ -22,7 +58,8 @@ for (let i = 0; i < imageArr.length; i++) {
 
   const image = document.createElement('img');
   image.setAttribute('class', 'gallery__image');
-  image.setAttribute('src', imageArr[i]);
+  image.setAttribute('src', imageArr[i].preview);
+  image.setAttribute('alt', imageArr[i].description);
 
   document.querySelector('ul.gallery').appendChild(item).appendChild(image);
 }
@@ -42,13 +79,16 @@ function openModal(e) {
   } else {
     target = e.target;
   }
-
   openImage(target);
 }
 
 function openImage(target) {
   const targetImage = target.getAttribute('src');
-  modalImage.setAttribute('src', targetImage);
+  let original = {};
+  for (const item of imageArr) {
+    if (item.preview === targetImage) original = item;
+  }
+  modalImage.setAttribute('src', original.original);
 }
 
 // close modal
@@ -67,13 +107,18 @@ backdrop.addEventListener('click', (e) => {
 // arrows
 document.addEventListener('keydown', (e) => {
   if (!backdrop.classList.contains('is-hidden')) {
-    const imageSource = imageArr.indexOf(modalImage.getAttribute('src'));
+    const targetImage = document.querySelector('.modal__image');
+    let imageSource = 0;
+    for (const item of imageArr) {
+      if (item.original === targetImage.getAttribute('src'))
+        imageSource = imageArr.indexOf(item);
+    }
     const imageIndex = imageArr.indexOf(imageArr[imageSource]);
 
     if (e.keyCode === 39 && imageIndex < imageArr.length - 1) {
-      modalImage.setAttribute('src', imageArr[imageSource + 1]);
+      modalImage.setAttribute('src', imageArr[imageSource + 1].original);
     } else if (e.keyCode === 37 && imageIndex > 0) {
-      modalImage.setAttribute('src', imageArr[imageSource - 1]);
+      modalImage.setAttribute('src', imageArr[imageSource - 1].original);
     }
   }
 });
